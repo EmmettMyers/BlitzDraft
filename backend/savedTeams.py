d35f3b1e238cfefc7949b71e722fc4ef22ea88f0
Emmett Myers <emmettleemyers@gmail.com>
2023-11-09 12:44:21 -0600

    Update savedTeams.py

--- backend/savedTeams.py
+++ backend/savedTeams.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://emmettmyers:dbCS8045@cluster0.bnt1l57.mongodb.net/?retryWrites=true&w=majority"
+uri = {MONGO_URI}
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["BlitzDraft"]
 col = db["savedTeams"]
